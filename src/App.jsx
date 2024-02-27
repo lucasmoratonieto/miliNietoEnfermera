b5b91defa25fba278ceb755e163f8ff7c5b5994c
lucasmoratonieto <lukmorato@gmail.com>
2024-02-27 19:19:50 +0100

    Starting page PC

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <div>mili Nieto foto</div>
+      <div>Quien soy?</div>
+      <div>Que cuidados hago?</div>
+      <div>Reviews de personas</div>
+      <div>contactos</div>
+      <div>Pie de páginas</div>
+    </>
+  )
+}
+
+export default App
