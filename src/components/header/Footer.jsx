ce10ae76decc7a0862bd7dd98a30f60f49d1cd94
Lucas Morato <lukmorato@gmail.com>
2024-04-16 18:47:26 +0200

    PC

--- src/components/header/Footer.jsx
+++ src/components/header/Footer.jsx
@@ -1,10 +1,16 @@
 
 import { navLinks } from '../../constants/index'
+import	{logoMili} from "../../assets/images/miliNietoEnfermeraLogo.jpg " 
 
 function Footer() {
   return (
-    <footer className=' flex justify-center font-palanquin' >
-      Hola
+    <footer className=' flex justify-center font-palanquin bg-[#3E3E3E] rounded-3xl' >
+      <div>
+        <div>
+          <img src={logoMili} />
+        </div>
+        <div></div>
+      </div>
     </footer>
   )
 }
