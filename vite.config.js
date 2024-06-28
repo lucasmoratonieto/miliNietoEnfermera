0cbacaed2cca672444ead078f38109e39cff15d3
Lucas Morato <lukmorato@gmail.com>
2024-06-28 08:49:19 +0200

    PC

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Default output directory
-    sourcemap: false, // Disable source maps for production
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Customize chunking if needed
-      },
-    },
-  },
-  define: {
-    'process.env.NODE_ENV': '"production"',
-  },
+  base: "https://lucasmoratonieto.github.io/miliNietoEnfermera/",
 })
