173dbf5a0dba7526e0056ef04fc2ce8b7ac1e42f
Lucas <lukmorato@gmail.com>
2025-02-23 08:53:21 +0100

    Mantenimiento en main

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    Mantenimiento
   </React.StrictMode>,
 )
 
